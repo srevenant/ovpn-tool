935d4bf0f69e4b15209c557861c7805ee826562f
s3tqr2w <brandon.gillespie@kuali.co>
2017-05-17 08:03:01 -0600

    release

--- release.sh
+++ release.sh
@@ -4,3 +4,8 @@ v=$(date +%y%M.%d%H)
 echo $v > ver
 
 sed -i -e 's/^VERSION = .*:VERSION:/VERSION = "'$v'" #:VERSION:/' vpnstart 
+
+git add ver vpnstart
+git commit -m 'release'
+
+echo "You should push origin"
